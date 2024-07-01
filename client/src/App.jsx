2fc2c9922f821c1206a6dbfd809234dca8656163
koridal <hjpark002@gmail.com>
2024-07-01 13:18:52 +0900

    Create Head section

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Header from './components/Header';
+import Contact from './pages/Contact';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,67 @@
+import { Navbar, TextInput, Button } from 'flowbite-react';
+import { Link, useLocation } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon } from 'react-icons/fa';
+
+export default function Header() {
+  const path = useLocation().pathname
+  return (
+    <Navbar className='border-b-2'>
+      <Link to='/' className='self-center whitespace-nowrap text-md sm:text-xl font-md dark:text-white tracking-tighter'>
+        <span className='px-2 py-1 rounded-lg bg-gradient-to-r from-blue-700 via-blue-900 to-blue-950 text-white font-semibold'>
+          Inko Jaya
+        </span>
+        &nbsp;Konstruksi
+      </Link>
+      <form>
+        <TextInput 
+          type='text'
+          placeholder='Search...'
+          rightIcon={AiOutlineSearch}
+          className='hidden lg:inline'
+        />
+      </form>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill >
+        <AiOutlineSearch />
+      </Button>
+      <div className='flex gap-2 md:order-2'>
+        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill >
+          <FaMoon />
+        </Button>
+        <Link to='/sign-in'>
+          <Button gradientDuoTone='purpleToBlue'>
+            Sign In
+          </Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+          <Navbar.Link active={path === '/'} as={'div'}>
+            <Link to='/'>
+              Home
+            </Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === '/about'} as={'div'}>
+            <Link to='/about'>
+              About
+            </Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === '/services'} as={'div'}>
+            <Link to='/services'>
+              Services
+            </Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === '/projects'} as={'div'}>
+            <Link to='/projects'>
+              Projects
+            </Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === '/contact'} as={'div'}>
+            <Link to='/contact'>
+              Contact
+            </Link>
+          </Navbar.Link>
+        </Navbar.Collapse>
+    </Navbar>
+  )
+}
