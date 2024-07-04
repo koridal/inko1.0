315380644bcc16906c19890b8429365699c152f4
koridal <hjpark002@gmail.com>
2024-07-04 21:57:53 +0900

    Add admin functionality to the user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -50,7 +50,8 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(400, 'Invalid password.'));
     }
     const token = jwt.sign(
-      { id: validUser._id }, process.env.JWT_SECRET);
+      { id: validUser._id, isAdmin: validUser.isAdmin }, 
+      process.env.JWT_SECRET);
 
     const { password: pass, ...rest } = validUser._doc;
 
