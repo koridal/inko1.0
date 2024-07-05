0e79a2fdc4ae52133b718cdf4ce5657837d8a14d
koridal <hjpark002@gmail.com>
2024-07-05 11:34:03 +0900

    Add posts section to the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
-// import DashPosts from '../components/DashPosts';
+import DashPosts from '../components/DashPosts';
 // import DashUsers from '../components/DashUsers';
 // import DashComments from '../components/DashComments';
 // import DashboardComp from '../components/DashboardComp';
@@ -32,7 +32,7 @@ export default function Dashboard() {
       {/* comments  */}
       {tab === 'comments' && <DashComments />}
       {/* dashboard comp */}
-      {tab === 'dash' && <DashboardComp />}
+      {/* {tab === 'dash' && <DashboardComp />} */}
     </div>
   );
 }
