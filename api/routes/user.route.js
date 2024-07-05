66a1f3354de86965f763548e4b42cefca6354f96
koridal <hjpark002@gmail.com>
2024-07-05 16:00:42 +0900

    add

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,6 +4,8 @@ import {
   updateUser,
   deleteUser,
   signout,
+  getUser,
+  getUsers
 } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
@@ -13,7 +15,7 @@ router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
-// router.get('/getusers', verifyToken, getUsers);
-// router.get('/:userId', getUser);
+router.get('/getusers', verifyToken, getUsers);
+router.get('/:userId', getUser);
 
 export default router;
