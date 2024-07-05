65e27c3f519480e6f8c413b0c85cfefc0b1bc8b2
koridal <hjpark002@gmail.com>
2024-07-05 16:32:17 +0900

    Show users to the admin dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,6 +1,6 @@
-import { Modal, Table, Button } from "flowbite-react";
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { Modal, Table, Button } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
 import { set } from 'mongoose';
@@ -72,10 +72,10 @@ export default function DashPosts() {
   };
 
   return (
-    <div className="table-auto overflow-x-scroll p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500 md:mx-auto">
+    <div className='table-auto overflow-x-scroll p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500 md:mx-auto'>
       {currentUser.isAdmin && userPosts.length > 0 ? (
         <>
-          <Table hoverable className="shadow-md">
+          <Table hoverable className='shadow-md'>
             <Table.Head>
               <Table.HeadCell>Date updated</Table.HeadCell>
               <Table.HeadCell>Post image</Table.HeadCell>
@@ -85,8 +85,8 @@ export default function DashPosts() {
               <Table.HeadCell><span>Edit</span></Table.HeadCell>
             </Table.Head>
             {userPosts.map((post) => (
-              <Table.Body className="divide-y">
-                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+              <Table.Body className='divide-y' key={post._id}>
+                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
                   <Table.Cell>
                     {new Date(post.updatedAt).toLocaleDateString()}
                   </Table.Cell>
@@ -95,13 +95,13 @@ export default function DashPosts() {
                       <img
                         src={post.image}
                         alt={post.title}
-                        className="h-10 w-20 bg-gray-500 object-cover"
+                        className='h-10 w-20 bg-gray-500 object-cover'
                       />
                     </Link>
                   </Table.Cell>
                   <Table.Cell>
                     <Link
-                      className="font-medium text-gray-900 dark:text-white"
+                      className='font-medium text-gray-900 dark:text-white'
                       to={`/post/${post.slug}`}
                     >
                       {post.title}
@@ -114,14 +114,14 @@ export default function DashPosts() {
                         setShowModal(true);
                         setPostIdToDelete(post._id);
                       }}
-                      className="cursor-pointer font-medium text-red-500 hover:underline"
+                      className='cursor-pointer font-medium text-red-500 hover:underline'
                     >
                       Delete
                     </span>
                   </Table.Cell>
                   <Table.Cell>
                     <Link
-                      className="text-teal-500 hover:underline"
+                      className='text-teal-500 hover:underline'
                       to={`/update-post/${post._id}`}
                     >
                       <span>Edit</span>
@@ -134,7 +134,7 @@ export default function DashPosts() {
           {showMore && (
             <button
               onClick={handleShowMore}
-              className="w-full self-center py-7 text-sm text-teal-500"
+              className='w-full self-center py-7 text-sm text-teal-500'
             >
               Show more
             </button>
@@ -147,20 +147,20 @@ export default function DashPosts() {
         show={showModal}
         onClose={() => setShowModal(false)}
         popup
-        size="md"
+        size='md'
       >
         <Modal.Header />
         <Modal.Body>
-          <div className="text-center">
-            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
-            <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
+          <div className='text-center'>
+            <HiOutlineExclamationCircle className='mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200' />
+            <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
               Are you sure you want to delete this post?
             </h3>
-            <div className="flex justify-center gap-4">
-              <Button color="failure" onClick={handleDeletePost}>
+            <div className='flex justify-center gap-4'>
+              <Button color='failure' onClick={handleDeletePost}>
                 Yes, I'm sure
               </Button>
-              <Button color="gray" onClick={() => setShowModal(false)}>
+              <Button color='gray' onClick={() => setShowModal(false)}>
                 No, cancel
               </Button>
             </div>
